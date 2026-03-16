085172f7a6887d3bb1a7409a4c9ebc00c7e0efc9
tuan-hda <93902080+tuan-hda@users.noreply.github.com>
2026-03-16 13:34:03 +0700

    Update percentage

--- src/constants/investment.ts
+++ src/constants/investment.ts
@@ -4,10 +4,10 @@ export const DEFAULT_CATEGORIES: Category[] = [
   {
     id: "stocks",
     name: "Quỹ cổ phiếu",
-    percentage: 30,
+    percentage: 65,
     subCategories: [
-      { id: "vesaf", name: "VESAF", share: 0.5 },
-      { id: "ssisca", name: "SSISCA", share: 0.5 },
+      { id: "dcds", name: "DCDS", share: 0.5 },
+      { id: "vcbf-bcf", name: "VCBF-BCF", share: 0.5 },
     ],
   },
   {
@@ -16,7 +16,7 @@ export const DEFAULT_CATEGORIES: Category[] = [
     percentage: 10,
     subCategories: [{ id: "vcbf-fif", name: "VCBF-FIF", share: 1 }],
   },
-  { id: "gold", name: "Vàng", percentage: 35 },
-  { id: "savings", name: "Tiết kiệm", percentage: 20 },
+  { id: "gold", name: "Vàng", percentage: 20 },
+  { id: "savings", name: "Tiết kiệm", percentage: 0 },
   { id: "bitcoin", name: "Bitcoin", percentage: 5 },
 ];
